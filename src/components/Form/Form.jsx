ba1b1651f92a5a3e135096f0c9ad36f2f7021f85
Elias Souza <97631394+eliasnsz@users.noreply.github.com>
2022-09-21 17:32:22 -0300

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,25 +8,25 @@ export default function Form() {
           <div className='input-container'>
             <label>
               Nome:
-              <input required name="name" id="name" autoComplete='name'/>
+              <input required name="Nome" id="name" autoComplete='name'/>
             </label>
           </div>
           <div className='input-container'>
             <label>
               Email:
-              <input required name="email" id="email" autoComplete='email' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
+              <input required name="Email" id="email" autoComplete='email' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
             </label>
           </div>
           <div className='input-container'>
             <label>
               Assunto:
-              <input required type="text" name="assunto" id="assunto" autoComplete='none'/>
+              <input required type="text" name="Assunto" id="assunto" autoComplete='none'/>
             </label>
           </div>
           <div className='input-container'>
             <label>
               Mensagem:
-              <textarea required name="msg" id="msg" col="10" rows="4" ></textarea>
+              <textarea required name="Mensagem" id="msg" col="10" rows="4" ></textarea>
             </label>
           </div>
           <button type="submit">Enviar</button>
