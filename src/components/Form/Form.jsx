0ff966454bbb44ef9f4ea3c297007bc86ecf68df
Elias Souza <97631394+eliasnsz@users.noreply.github.com>
2022-09-21 20:03:04 -0300

    .

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,25 +8,25 @@ export default function Form() {
           <div className='input-container'>
             <label>
               Nome:
-              <input required name="Nome" id="name" autoComplete='name'/>
+              <input placeholder='Qual o seu nome?' required name="Nome" id="name" autoComplete='name'/>
             </label>
           </div>
           <div className='input-container'>
             <label>
               Email:
-              <input required name="Email" id="email" autoComplete='email' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
+              <input placeholder='Qual o seu email?' required name="Email" id="email" autoComplete='email' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"/>
             </label>
           </div>
           <div className='input-container'>
             <label>
               Assunto:
-              <input required type="text" name="Assunto" id="assunto" autoComplete='none'/>
+              <input required placeholder='' type="text" name="Assunto" id="assunto" autoComplete='none'/>
             </label>
           </div>
           <div className='input-container'>
             <label>
               Mensagem:
-              <textarea required name="Mensagem" id="msg" col="10" rows="4" ></textarea>
+              <textarea required name="Mensagem" id="msg" col="10" rows="3" ></textarea>
             </label>
           </div>
           <button type="submit">Enviar</button>
