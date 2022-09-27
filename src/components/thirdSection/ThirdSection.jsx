6e7e8463afa8e5e14d909d5b853341bb2d4f7a9a
Elias Souza <97631394+eliasnsz@users.noreply.github.com>
2022-09-27 17:27:25 -0300

    Adição de Projeto
    Adição do Valorant Wiki na lista de projetos

--- src/components/thirdSection/ThirdSection.jsx
+++ src/components/thirdSection/ThirdSection.jsx
@@ -20,22 +20,22 @@ export default function ThirdSection() {
               <Card 
                 dataAos={"fade-up-right"}
                 img={"https://i.imgur.com/iKS9ybE.jpg"}
-                year={"2020"}
+                year={"2022"}
                 title={"Creative Cloud Clone"}
                 desc={`Clone totalmente responsivo do website da Creative Cloud, 
                   pertencente à Adobe. Esse projeto foi feito com a intenção de me 
-                  desafiar e testar meus conhecimentos, aprendi muito com ele.`}
+                  desafiar e aprimorar meus conhecimentos sobre ReactJS e design. Aprendi muito com ele.`}
                 hrefGit={"https://github.com/eliasnsz/creative-cloud-website-clone"}
                 hrefProject={"https://creative-cloud-website-clone.vercel.app/"}
               />
               <Card
                 dataAos={"fade-up-left"}
-                img={"https://i.imgur.com/FcuT1Su.png"}
-                year={"----"}
-                title={"Em breve"}
-                desc={`...`}
-                hrefGit={"#"}
-                hrefProject={"#"}
+                img={"https://i.imgur.com/t07qxhU.jpg"}
+                year={"2022 (Em Desenvolvimento)"}
+                title={"Valorant Wiki"}
+                desc={`Criado com ReactJS e sem back-end, o projeto conta com páginas geradas dinâmicamente fazendo consumo da Valorant-API. Iniciei esse projeto com objetivo de evoluir minhas habilidades com consumo de API.`}
+                hrefGit={"https://github.com/eliasnsz/valorant-wiki"}
+                hrefProject={"https://valorant-wiki-zhtd.vercel.app/"}
               />
               <Card
                 dataAos={"fade-up-right"}
